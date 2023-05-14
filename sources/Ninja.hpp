620e038811aefffcfd1914d374d1d48a7d27a127
Naama Shiponi <naamas1234567@gmail.com>
2023-05-14 23:21:36 +0300

    fix problem for tidy

--- sources/Ninja.hpp
+++ sources/Ninja.hpp
@@ -22,14 +22,14 @@ namespace ariel
         static const int YOUNG_NINJA_HITPOINTS = 100;
         static const int TRAINED_NINJA_SPEED = 12;
         static const int TRAINED_NINJA_HITPOINTS = 120;
-        
-        Ninja(const std::string &name, const Point &location, int speed = DEFAULT_SPEED, int hitPoints = DEFAULT_HITPOINTS)
+
+        Ninja(const std::string &name,int speed , const Point &location,  int hitPoints )
             : Character(name, location, hitPoints), speed(speed), hitPoints(hitPoints) {}
         Ninja(const Ninja &) = delete;
         Ninja &operator=(const Ninja &) = delete;
         Ninja(Ninja &&) = delete;
         Ninja &operator=(Ninja &&) = delete;
-
+        ~Ninja(){};
         int getSpeed() const { return speed; }
         int getHitPoints() const { return hitPoints; }
         void move(Character *enemy);
@@ -40,21 +40,21 @@ namespace ariel
     {
     public:
         OldNinja(const std::string &name, const Point &location)
-            : Ninja(name, location, OLD_NINJA_SPEED, OLD_NINJA_HITPOINTS) {}
+            : Ninja(name, OLD_NINJA_SPEED,location,  OLD_NINJA_HITPOINTS) {}
     };
 
     class YoungNinja : public Ninja
     {
     public:
         YoungNinja(const std::string &name, const Point &location)
-            : Ninja(name, location, YOUNG_NINJA_SPEED, YOUNG_NINJA_HITPOINTS) {}
+            : Ninja(name,YOUNG_NINJA_SPEED, location,  YOUNG_NINJA_HITPOINTS) {}
     };
 
     class TrainedNinja : public Ninja
     {
     public:
         TrainedNinja(const std::string &name, const Point &location)
-            : Ninja(name, location, TRAINED_NINJA_SPEED, TRAINED_NINJA_HITPOINTS) {}
+            : Ninja(name,TRAINED_NINJA_SPEED, location,  TRAINED_NINJA_HITPOINTS) {}
     };
 
 }
